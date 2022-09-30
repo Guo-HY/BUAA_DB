1450aef2762fae1008885c24cd75ef0ad97fb15f
ghy <2247883756@qq.com>
2022-09-30 19:41:10 +0800

    add post

--- buaa_db/backend/views.py
+++ buaa_db/backend/views.py
@@ -31,6 +31,29 @@ class StudentLogin(APIView):
     else:
       return Response(dict([('status', 'student_id_not_found')]))
     
+  # def post(self, request):
+  #   print("---StudentLogin---")
+  #   student_id = str(request.POST.get('student_id'))
+  #   student_password = str(request.POST.get('student_password'))
+  #   print(student_id + ' ' + student_password)
+    
+  #   sql = Mysql()
+  #   result = sql.studentLogin(student_id, student_password)
+  #   flag = not not result
+    
+  #   if flag:
+  #     print(result)
+  #   else:
+  #     print('not found')
+    
+  #   if flag:
+  #     if student_password == result[0][2]:
+  #       return Response(dict([('status', 'success')]))
+  #     else:
+  #       return Response(dict([('status', 'student_password_wrong')]))
+  #   else:
+  #     return Response(dict([('status', 'student_id_not_found')]))
+    
 class StudentRegister(APIView):
   def get(self, request):
     print("---StudentRegister---")
@@ -47,9 +70,28 @@ class StudentRegister(APIView):
                                  student_grade, student_class)
     
     if result:
-      return Response(dict(['status', 'success']))
+      return Response(dict([('status', 'success')]))
     else:
-      return Response(dict(['status', 'fail']))
+      return Response(dict([('status', 'fail')]))
+    
+  # def post(self, request):
+  #   print("---StudentRegister---")
+  #   student_id = str(request.POST.get('student_id', None))
+  #   student_password = str(request.POST.get('student_password', None))
+  #   student_username = str(request.POST.get('student_username', None))
+  #   student_grade = str(request.POST.get('student_grade', None))
+  #   student_class = str(request.POST.get('student_class', None))
+  #   print(student_id + ' ' + student_password + ' ' + student_username
+  #         + ' ' + student_grade + ' ' + student_class)
+    
+  #   sql = Mysql()
+  #   result = sql.studentRegister(student_id, student_password, student_username, 
+  #                                student_grade, student_class)
+    
+  #   if result:
+  #     return Response(dict([('status', 'success')]))
+  #   else:
+  #     return Response(dict([('status', 'fail')]))
   
 class StudentCurriculum(APIView):
   def get(self, request):
@@ -63,7 +105,20 @@ class StudentCurriculum(APIView):
     for item in result:
       courselist.append({'course_id':item[0], 'course_name':item[1], 
                          'course_intro':item[2], 'course_capacity':item[3], 'course_elected':item[4]})
-    return Response(courselist)
+    return Response(dict([("classes", courselist)]))
+  
+  # def post(self, request):
+  #   print("---StudentCurriculum---")
+  #   student_id = str(request.POST.get('student_id', None))
+  #   print(student_id)
+  #   sql = Mysql()
+  #   result = sql.studentCurriculum(student_id)
+  #   courselist = []
+  #   print(result)
+  #   for item in result:
+  #     courselist.append({'course_id':item[0], 'course_name':item[1], 
+  #                        'course_intro':item[2], 'course_capacity':item[3], 'course_elected':item[4]})
+  #   return Response(dict([("classes", courselist)]))
   
 class StudentCourseSelection(APIView):
   def get(self, request):
@@ -77,6 +132,17 @@ class StudentCourseSelection(APIView):
     else:
       return Response(dict([('status', 'fail')]))
     
+  # def post(self, request):
+  #   print("---StudentCourseSelection---")
+  #   student_id = str(request.POST.get('student_id', None))
+  #   course_id = str(request.POST.get('course_id', None))
+  #   sql = Mysql()
+  #   result = sql.studentCourseSelection(student_id, course_id)
+  #   if result:
+  #     return Response(dict([('status', 'success')]))
+  #   else:
+  #     return Response(dict([('status', 'fail')]))
+    
 class StudentCoursePost(APIView):
   def get(self, request):
     print("---StudentCoursePost---")
@@ -88,13 +154,37 @@ class StudentCoursePost(APIView):
     for item in result:
       courselist.append({'course_id':item[0], 'course_name':item[1], 
                          'course_intro':item[2], 'course_capacity':item[3], 'course_elected':item[4]})
-    return Response(courselist)
+    return Response(dict([("classes", courselist)]))
+  
+  # def post(self, request):
+  #   print("---StudentCoursePost---")
+  #   student_id = str(request.POST.get('student_id', None))
+  #   sql = Mysql()
+  #   result = sql.studentCoursePost(student_id)
+  #   courselist = []
+  #   print(result)
+  #   for item in result:
+  #     courselist.append({'course_id':item[0], 'course_name':item[1], 
+  #                        'course_intro':item[2], 'course_capacity':item[3], 'course_elected':item[4]})
+  #   return Response(dict([("classes", courselist)]))
   
 class StudentCourseWithdraw(APIView):
   def get(self, request):
     print("---StudentCourseWithdraw---")
     student_id = str(request.GET.get('student_id', None))
     course_id = str(request.GET.get('course_id', None))
+    sql = Mysql()
+    result = sql.studentCourseWithdraw(student_id, course_id)
+    # if result:
+    #   return Response(dict(['status', 'success']))
+    # else:
+    #   return Response(dict(['status', 'fail']))
+    return Response(dict([('status', 'success')]))
+  
+  def post(self, request):
+    print("---StudentCourseWithdraw---")
+    student_id = str(request.POST.get('student_id', None))
+    course_id = str(request.POST.get('course_id', None))
     sql = Mysql()
     result = sql.studentCourseWithdraw(student_id, course_id)
     # if result:
