3a9f4d569147f19c3f1cf3e1521ca9a4d3adf035
ghy <2247883756@qq.com>
2022-11-06 13:37:22 +0800

    finisl database.sql

--- src/backend/database.sql
+++ src/backend/database.sql
@@ -1,58 +1,175 @@
-CREATE DATABASE if not exists buaa_db1 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE buaa_db1;
+DROP DATABASE buaa_db2;
+CREATE DATABASE if not exists buaa_db2 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE buaa_db2;
 
-DROP TABLE IF EXISTS `student_course`;
-DROP TABLE IF EXISTS `students`;
-CREATE TABLE `students`
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `group`;
+DROP TABLE IF EXISTS `post`;
+DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `bottle`;
+DROP TABLE IF EXISTS `bottle_reply`;
+DROP TABLE IF EXISTS `activity`;
+DROP TABLE IF EXISTS `message`;
+DROP TABLE IF EXISTS `label`;
+DROP TABLE IF EXISTS `user_like_comment`;
+DROP TABLE IF EXISTS `user_like_post`;
+DROP TABLE IF EXISTS `user_label`;
+DROP TABLE IF EXISTS `user_user`;
+DROP TABLE IF EXISTS `user_activity`;
+
+CREATE TABLE `user`
 (
-  `student_id` INT(20) PRIMARY KEY NOT NULL,
-  `student_username` VARCHAR(255) NOT NULL,
-  `student_password` VARCHAR(255) NOT NULL,
-  `student_grade` INT(20) NOT NULL,
-  `student_class` INT(20) NOT NULL
+  `user_id`       INT(20)       NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `name`          VARCHAR(255)  NOT NULL UNIQUE ,
+  `password`      VARCHAR(255)  NOT NULL ,
+  `head_portrait` VARCHAR(255) ,
+  `contact`       VARCHAR(255)
+
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-DROP TABLE IF EXISTS `courses`;
--- 创建课程表
-CREATE TABLE `courses`
+CREATE TABLE `group`
 (
-  `course_id` INT(20) PRIMARY KEY NOT NULL,
-  `course_name` VARCHAR(255) NOT NULL,
-  `course_intro` VARCHAR(10000) DEFAULT NULL,
-  `course_capacity` INT(20) NOT NULL,
-  `course_elected` INT(20) NOT NULL DEFAULT 0,
-  CHECK(`course_elected` <= `course_capacity`)
+  `group_id`          INT(20)       NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `post_num`          INT(20)       NOT NULL ,
+  `group_name`        VARCHAR(255)  NOT NULL ,
+  `group_description` VARCHAR(1000) NOT NULL ,
+  `user_id`           INT(20)       NOT NULL ,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`)
+
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `student_course`;
--- 创建学生选课表 
-CREATE TABLE `student_course`
+CREATE TABLE `post`
 (
-  `student_id` INT(20) NOT NULL,
-  `course_id` INT(20) NOT NULL,
-  FOREIGN KEY(`student_id`) REFERENCES `students`(`student_id`),
-  FOREIGN KEY(`course_id`) REFERENCES `courses`(`course_id`),
-  PRIMARY KEY(`student_id`, `course_id`)
+  `post_id`     INT(20)         NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `name`        VARCHAR(255)    NOT NULL ,
+  `context`     VARCHAR(10000)  NOT NULL ,
+  `post_time`   VARCHAR(255)    NOT NULL ,
+  `comment_num` INT(20)         NOT NULL ,
+  `likes_num`   INT(20)         NOT NULL ,
+  `user_id`     INT(20)         NOT NULL ,
+  `group_id`    INT(20)         NOT NULL ,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`) ,
+  FOREIGN KEY(`group_id`) REFERENCES `group`(`group_id`)
+
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-  
-INSERT INTO `students` 
-(student_id, student_username, student_password, student_grade, student_class)
-VALUES
-(1, 'ghy', '123ghy', 1, 1),
-(2, 'zal', '123zal', 2, 2),
-(3, 'zlb', '123zlb', 3, 3);
 
-INSERT INTO `courses` 
-(course_id, course_name, course_intro, course_capacity, course_elected)
-VALUES
-(1, 'A', 'aaa', 2, 0),
-(2, 'B', 'bbb', 2, 0),
-(3, 'C', 'ccc', 4, 0);
+CREATE TABLE `comment`
+(
+  `comment_id`    INT(20)         NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `context`       VARCHAR(10000)  NOT NULL ,
+  `comment_time`  VARCHAR(255)    NOT NULL ,
+  `likes_num`     INT(20)         NOT NULL ,
+  `user_id`       INT(20)         NOT NULL ,
+  `post_id`       INT(20)         NOT NULL ,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`) ,
+  FOREIGN KEY(`post_id`) REFERENCES `post`(`post_id`)
 
-INSERT INTO `student_course` 
-(student_id, course_id)
-VALUES
-(1, 1),
-(2, 2),
-(3, 3);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `bottle`
+(
+  `bottle_id` INT(20)         NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `context`   VARCHAR(10000)  NOT NULL ,
+  `user_id`   INT(20)         NOT NULL ,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`) 
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `bottle_reply`
+(
+  `bottle_reply_id` INT(20)         NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `context`         VARCHAR(10000)  NOT NULL ,
+  `user_id`         INT(20)         NOT NULL ,
+  `bottle_id`       INT(20)         NOT NULL ,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`) ,
+  FOREIGN KEY(`bottle_id`) REFERENCES `bottle`(`bottle_id`)
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `activity`
+(
+  `activity_id` INT(20)         NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `summary`     VARCHAR(1000)   NOT NULL ,
+  `begin_time`  VARCHAR(255)    NOT NULL ,
+  `end_time`    VARCHAR(255)    NOT NULL ,
+  `location`    VARCHAR(255)    NOT NULL ,
+  `user_id`     INT(20)         NOT NULL ,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`) 
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE  `message`
+(   
+  `message_id`  INT(20)         NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `context`     VARCHAR(10000)  NOT NULL ,
+  `send_time`   VARCHAR(255)    NOT NULL ,
+  `send_id`     INT(20)         NOT NULL ,
+  `receive_id`  INT(20)         NOT NULL ,
+  FOREIGN KEY(`send_id`) REFERENCES `user`(`user_id`) ,
+  FOREIGN KEY(`receive_id`) REFERENCES `user`(`user_id`) 
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE  `label`
+(
+  `label_id`    INT(20)       NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `label_name`  VARCHAR(255)  NOT NULL 
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE  `user_like_comment`
+(
+  `user_id`     INT(20)       NOT NULL,
+  `comment_id`  INT(20)       NOT NULL,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`) ,
+  FOREIGN KEY(`comment_id`) REFERENCES `comment`(`comment_id`) ,
+  PRIMARY KEY(`user_id`, `comment_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE  `user_like_post`
+(
+  `user_id` INT(20)       NOT NULL , 
+  `post_id` INT(20)       NOT NULL , 
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`) ,
+  FOREIGN KEY(`post_id`) REFERENCES `post`(`post_id`) ,
+  PRIMARY KEY(`user_id`, `post_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE  `user_label`
+(
+  `user_id`   INT(20)       NOT NULL ,  
+  `label_id`  INT(20)       NOT NULL , 
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`) ,
+  FOREIGN KEY(`label_id`) REFERENCES `label`(`label_id`) ,
+  PRIMARY KEY(`user_id`, `label_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE  `group_label`
+(
+  `group_id`  INT(20)       NOT NULL ,
+  `label_id`  INT(20)       NOT NULL ,
+  FOREIGN KEY(`group_id`) REFERENCES `group`(`group_id`) ,
+  FOREIGN KEY(`label_id`) REFERENCES `label`(`label_id`) ,
+  PRIMARY KEY(`group_id`, `label_id`)
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE  `user_user`
+(
+  `user1_id` INT(20)       NOT NULL ,
+  `user2_id` INT(20)       NOT NULL ,
+  FOREIGN KEY(`user1_id`) REFERENCES `user`(`user_id`) ,
+  FOREIGN KEY(`user2_id`) REFERENCES `user`(`user_id`) ,
+  PRIMARY KEY(`user1_id`, `user2_id`)
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE  `user_acitivity`
+(
+  `user_id`     INT(20)       NOT NULL ,
+  `activity_id` INT(20)       NOT NULL ,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`) ,
+  FOREIGN KEY(`activity_id`) REFERENCES `activity`(`activity_id`) ,
+  PRIMARY KEY(`user_id`, `activity_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
